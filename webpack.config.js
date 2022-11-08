20ec40a2d0f8fe950de4d45dc5a415676c1b1eaa
Matthias Leuffen <matthes@leuffen.de>
2022-11-08 21:23:10 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: {
         '_includes/dist/style_embed': './src/style_embed.scss',
-        'assets/dist/index': './src/style.scss',
+        'assets/dist/style': './src/style.scss',
         'assets/dist/index': './src/index.ts',
         '_includes/dist/index_embed': './src/index_embed.ts'
     },
